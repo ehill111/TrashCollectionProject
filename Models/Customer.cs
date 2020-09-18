6024afce366f0bad59624797353253f4891b54f4
Eric M Hill <ehill111@comcast.net>
2020-09-18 17:22:03 -0400

    Added properties for Customer class.

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -7,5 +7,16 @@ namespace TrashCollectionRiches.Models
 {
     public class Customer
     {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public string Email { get; set; }
+        public string PickUpDay { get; set; }
+        public string PickUpStatus { get; set; }
+        public string PaymentStatus { get; set; }
     }
 }
--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,14 @@ namespace TrashCollectionRiches.Models
 {
     public class Employee
     {
+        [Key]
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        
     }
 }
