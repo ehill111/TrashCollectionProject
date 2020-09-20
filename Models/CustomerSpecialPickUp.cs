879dda4736c69037bbe8698e142ea272f23c7f42
Eric M Hill <ehill111@comcast.net>
2020-09-19 20:50:26 -0400

    Created RecordOfPickUp model and added properties.

--- Models/CustomerSpecialPickUp.cs
+++ Models/CustomerSpecialPickUp.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace TrashCollectionRiches.Models
-{
-    public class CustomerSpecialRequest
+{   
+    public class CustomerSpecialPickUp
     {
+        [Key]
+        public bool SpecialPickUp { get; set; }
+        public DateTime SpecialPickUpDate { get; set; }
+        public bool SpecialPickUpCancel { get; set; }
     }
 }
