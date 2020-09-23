e90f7d0c9d70681d441ebea1dc79de3124be6309
Eric M Hill <ehill111@comcast.net>
2020-09-23 07:49:00 -0400

    Created GET and POST methods to enable customer to request an extra, one-time pickup.

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -63,11 +63,11 @@ namespace TrashCollectionRiches.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction("Index");
         }
+        //Customer able to see how much is owed.
         //GET: Customer/Details/5
         public async Task<IActionResult> Details(int id)
         {
             Customer Balance = _context.Customer.Where(b => b.Balance == id).SingleOrDefault();
-
             return View(Balance);
         }
 
