9c5f96b1fe9e3bdef6ed86da66980ca86c3205ca
Eric M Hill <ehill111@comcast.net>
2020-09-24 07:01:52 -0400

    Added Display Attribute to Employee model.

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -9,13 +9,19 @@ namespace TrashCollectionRiches.Models
     public class Employee
     {
         [Key]
+        [Display(Name = "Employee Id", Order = -9)]
         public int EmployeeId { get; set; }
+        [Display(Name = "First Name", Order = -9)]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name", Order = -9)]
         public string LastName { get; set; }
+        [Display(Name = "Street Address", Order = -9)]
         public string StreetAddress { get; set; }
         public string City { get; set; }
         public string State { get; set; }
+        [Display(Name = "Zip Code", Order = -9)]
         public int ZipCode { get; set; }
+        [Display(Name = "Identity User Id", Order = -9)]
         public string IdentityUserId { get; internal set; }
     }
 }
