f57f21c7de3c5a28e13ce913a0d71c29722cbc66
Eric M Hill <ehill111@comcast.net>
2020-09-24 06:58:21 -0400

    Added Display Attribute to Customer model.

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -10,16 +10,24 @@ namespace TrashCollectionRiches.Models
     {
         [Key]
         public int CustomerId { get; set; }
+        [Display(Name = "First Name", Order = -9)]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name", Order = -9)]
         public string LastName { get; set; }
+        [Display(Name = "Street Address", Order = -9)]
         public string StreetAddress { get; set; }
         public string City { get; set; }
         public string State { get; set; }
+        [Display(Name = "Zip Code", Order = -9)]
         public int ZipCode { get; set; }
         public int Balance {get; set;}
+        [Display(Name = "PickUp Day", Order = -9)]
         public string PickUpDay { get; set; }
+        [Display(Name = "Special PickUp Date", Order = -9)]
         public DateTime SpecialPickUpDate { get; set; }
+        [Display(Name = "Suspend Start", Order = -9)]
         public DateTime SuspendStart { get; set; }
+        [Display(Name = "Suspend Stop", Order = -9)]
         public DateTime SuspendStop { get; set; }
 
     }
